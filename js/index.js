0aa68d11ba4b89819a25ddbc92c3275409bb7498
Anusha Chaturvedi <68815162+anusha-c18@users.noreply.github.com>
2022-05-24 14:05:48 +0530

    responsive, footer, about us, contact us form

--- js/index.js
+++ js/index.js
@@ -17,15 +17,22 @@ function currentSlide(n) {
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
+  for (i = 0; i < slides.length; i++) {   
       slides[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
   slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
 }
+//popup
+
+var x=document.getElementById("cross");
+var pop=document.getElementById("pop");
+
+function redirect(){
+  window.location.replace("http://127.0.0.1:5500/index.html");
+}
+
+x.addEventListener('click',redirect);
+
+
